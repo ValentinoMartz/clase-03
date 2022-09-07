f1dce4bb3a14cb2527eefbe6ca81eb4dd51133dd
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-07 11:21:57 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,34 +1,11 @@
-const myDoc = document;
+let myH3 = document.getElementById("mySuperH3");
 
-console.log(myDoc);
-
-let myH1 = myDoc.getElementsByTagName("h1");
-
-let myH3 = myDoc.getElementById("mySuperH3");
-
-console.log(myH1);
-console.log(myH3);
-
-myH3.innerText = "EStoy modificando el texto dentro del H3";
-
-const myButtonStr = "<button>Esto es un boton </button> </br>";
+const myButtonStr = `<button class="bg-success text-light">Esto es un boton </button> </br>`;
 
 myH3.innerHTML = myButtonStr;
 
-// While, do...while... for
+const cant_elements = prompt("Ingrese la cantidad de repeticiones");
 
-const cant_elements = 15;
-
-for (let index = 0; index < cant_elements; index++) {
-    myH3.innerHTML = myH3.innerHTML + myButtonStr;
+for (let index = 1; index < cant_elements; index++) {
+  myH3.innerHTML = myH3.innerHTML + myButtonStr;
 }
-
-/*
-const myObj = {
-    "atributo" : "valor"
-}
-
-const myObjeto2 = {
-    atributo: "valor",
-    atributo2: "otro vaalor"
-}*/
